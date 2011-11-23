3749905319a2861d00023bcc8f53fa446887dcab
Madmallard <mjjones0@gmail.com>
2011-11-22 20:57:41 -0800

    Fixed display so that it has the base case of functionality

--- FutureOfRampart/Game.cpp
+++ FutureOfRampart/Game.cpp
@@ -35,7 +35,12 @@ void Window::idleCallback()
 
 void Window::displayCallback()
 {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+  // DRAW EVERYTHING
+
+
+  glutSwapBuffers();
 }
 
 int main(int argc, char *argv[])
